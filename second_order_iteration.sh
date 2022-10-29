8c53f800c0a8070a4973064722ff26cc3fd296d2
hw454 <hayleywragg@hotmail.com>
2022-10-29 19:43:16 +0100

    Scripts for running the simulations through the different parameter sets.

---
+++ second_order_iteration.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Keith Briggs 2020-11-02 & Hayley Wragg
+# bash Raytracer_transmittermover.sh
+for ((job=0; job<=20; job++)); do
+    echo "job=${job}"
+    time python3 FEMEulerSemiImplicitSecondOrder.py "${job}"
+done
+
