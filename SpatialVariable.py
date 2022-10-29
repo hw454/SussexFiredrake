92bfd383378ce948290f6925dab5fbd0b8264e79
hw454 <hayleywragg@hotmail.com>
2022-10-29 19:45:10 +0100

    Interpolation updated to take the function at line.

--- SpatialVariable.py
+++ SpatialVariable.py
@@ -1,9 +1,10 @@
 from firedrake import *
 import numpy as np
-
 class spatial_variable:
     def __init__(s,V,r_mesh,name=''):
         s.var=Function(V)
+        s.init_var=Function(V)
+        s.var_hist=[]
         s.name=name
         s.mesh=r_mesh
         s.function_space=V
@@ -23,7 +24,7 @@ class spatial_variable:
     def get_function_space(s):
         return s.function_space
     def var_vals(s):
-        return s.var.vector.array[:]
+        return s.var.vector()
     def print_var_vals(s):
         return str(s.var_vals())
     def __str__(s):
@@ -31,33 +32,37 @@ class spatial_variable:
         rstr+=' Function Value '+s.print_var_vals()
         return rstr
     def update_var(s,cls_2):
-        s.var.vector.array[:]=cls_2.var.vector.array[:]
+        s.var.assign(cls_2.var)
+        s.var_hist.append(cls_2.var.copy(deepcopy=True))
         return
     def reset_var(s):
         s.var=Function(s.function_space)
         return s.var
-    def initialise(s,Steady,permval,char='n'):
+    def initialise(s,Steady,permval,hx,hy,char='n'):
         #perm and StSt must be declared as global
         global StSt,perm
         StSt=Steady
         perm=permval
+        x, y = SpatialCoordinate(s.mesh)
         if char=='n':
             print('Character key not input so using class key',s.char_key)
             char=s.char_key
         if char=='e':
-            s.var.interpolate(rand_scal_eta)
+            s.var.interpolate(StSt[0]+perm*(sin(pi*x/(2*hx))+sin(pi*y/(2*hy))))
         elif char=='t':
-            s.var.interpolate(rand_scal_theta)
+            s.var.interpolate(StSt[1]+perm*(sin(pi*x/(2*hx))+sin(pi*y/(2*hy))))
         else:
             print('No initial func for character key')
-            s.var.interpolate(initial_func)
+            s.var.interpolate(np.exp(-5*(x[0]**2+x[1]**2)))
+        s.init_var.assign(s.var)
+        s.var_hist.append(s.init_var.copy(deepcopy=True))
         return s.var
         
 
 def rand_scal_eta(x):
-    return StSt[0]+perm*(np.random.rand(x.shape[1])-np.random.rand(x.shape[1]))
+    return (StSt[0]+perm*(np.random.rand()-np.random.rand()))*x/x
 def rand_scal_theta(x):
-    return StSt[1]+perm*(np.random.rand(x.shape[1])-np.random.rand(x.shape[1]))
+    return (StSt[1]+perm*(np.random.rand()-np.random.rand()))*x/x
 def Initial_eta_func(out):
     #perm and StSt must be declared as global
     out.interpolate(rand_scal_eta)
