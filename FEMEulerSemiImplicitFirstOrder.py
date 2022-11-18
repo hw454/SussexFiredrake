9daf90965725c7715a3a372d0d1c77141940aa01
hw454 <hayleywragg@hotmail.com>
2022-11-18 20:52:43 +0000

    Set constants to be constants over the whole mesh. Separate input constants for the diffusion term for each variable. Solver parameters set.

--- FEMEulerSemiImplicitFirstOrder.py
+++ FEMEulerSemiImplicitFirstOrder.py
@@ -34,28 +34,29 @@ def check_steps(meshpars,dt):
     return check
 
 
-def f_func(r_mesh,eta,theta,consts_dict,test=0):
-    eps=consts_dict['eps']
+def f_func(eta,theta,consts_dict,test=0):
+    eps=Constant(consts_dict['eps'])
     #Eps=fem.Constant(r_mesh, PETSc.ScalarType(eps))
     #return - eta*theta+eps*eta**2/(1+eta) 
     # Pure Diffusion (Heat Eqn)
-    if test:
-        return 0 #fem.Constant(r_mesh, PETSc.ScalarType(0))
-    else:
-        return - eta*theta+(eps*eta**2/(1+eta))
-def g_func(r_mesh,eta,theta,consts_dict,test=0):
-    eps=consts_dict['eps']
-    a0=consts_dict['a0']
-    A0=consts_dict['A0']
-    b0=consts_dict['b0']
-    #Eps  =fem.Constant(r_mesh, PETSc.ScalarType(eps))
-    #a0cst=fem.Constant(r_mesh, PETSc.ScalarType(a0))
-    #b0cst=fem.Constant(r_mesh, PETSc.ScalarType(b0))
-    #A0cst=fem.Constant(r_mesh, PETSc.ScalarType(A0))
     if test==1:
         return 0 #fem.Constant(r_mesh, PETSc.ScalarType(0))
     else:
-        return A0*ufl.exp(a0*eta+b0*theta)*(1-theta*(1+((2**(1-eps*eta))/eps)))
+        ec=eta#conditional(eta>0.0,eta,0.0)
+        tc=theta#conditional(theta>0.0,theta,0.0)
+        return - ec*tc+(eps*ec**2/(1+ec))
+def g_func(eta,theta,consts_dict,test=0):
+    eps=Constant(consts_dict['eps'])
+    a0=Constant(consts_dict['a0'])
+    A0=Constant(consts_dict['A0'])
+    b0=Constant(consts_dict['b0'])
+    ec=eta#conditional(eta>0.0,eta,0.0)
+    tc=theta#conditional(theta>0.0,theta,0.0)
+    if test==1:
+        print('Heat eqn test')
+        return 0 #fem.Constant(r_mesh, PETSc.ScalarType(0))
+    else:
+        return A0*exp(a0*ec+b0*tc)*(1-tc*(1+((2**(1-eps*ec))/eps)))
 def linear_form_func(varkey,var_cls,othervar_cls,testvar,consts_dict,test=0):
     '''The function `linear_form_func` defines the linear form for the variational problem. 
     By defining this outside the main simulation the same main program can be used for 
@@ -72,24 +73,22 @@ def linear_form_func(varkey,var_cls,othervar_cls,testvar,consts_dict,test=0):
     :rtype: `ufl` expression
     :return: L=linear form
     '''
-    rho=consts_dict['rho']
-    sig=consts_dict['sig']
-    d=consts_dict['d']
-    dt=consts_dict['dt']
+    rho=Constant(consts_dict['rho'])
+    sig=Constant(consts_dict['sig'])
+    dt=Constant(consts_dict['dt'])
     othervar=othervar_cls.var
     initvar=var_cls.var
-    r_mesh=var_cls.mesh
-    print('Heateqn test',test)
-    divterm=initvar*testvar
+    c0=initvar#conditional(initvar>0.0,initvar,0.0)
+    divterm=c0*testvar
     if varkey=='e':
-        L=(dt*rho*f_func(r_mesh,initvar,othervar,consts_dict,test)*testvar
-           +divterm)*ufl.dx
+        L=(dt*rho*f_func(initvar,othervar,consts_dict,test)*testvar
+           +divterm)
     if varkey=='t':
-        L=(dt*sig*g_func(r_mesh,initvar,othervar,consts_dict,test)*testvar
-           +divterm)*ufl.dx
-    return L
+        L=(dt*sig*g_func(initvar,othervar,consts_dict,test)*testvar
+           +divterm)
+    return L*ufl.dx
 
-def bilinear_form_func(varkey,tmpvar,testvar,consts_dict,r_mesh,test=0):
+def bilinear_form_func(varkey,tmpvar,testvar,consts_dict,test=0):
     ''' bilinear form for variational problem.
     :param varkey: either 'e' or 't' this gives which variable to take the form form.
     :param tmpvar: is a `dolfinx.ufl` TrialFunction :math:`v`
@@ -109,19 +108,20 @@ def bilinear_form_func(varkey,tmpvar,testvar,consts_dict,r_mesh,test=0):
     :returns: a = bilinear form
     '''
     # bilinear form -trial function must go before test function
-    d=consts_dict['d']
-    dt=consts_dict['dt']
+    dt=Constant(consts_dict['dt'])
     #Dt =fem.Constant(r_mesh, PETSc.ScalarType(dt))
     if test==2:
-        al=0
+        print('Source only test')
+        al=0.0
     else:
-        al=1
+        al=1.0
     if varkey=='e':
-        D=1.0*al#fem.Constant(r_mesh, PETSc.ScalarType(1.0))
+        d=consts_dict['d_e']
     elif varkey=='t':
-        D=d*al#D  =fem.Constant(r_mesh, PETSc.ScalarType(d))
-    a=D*dt*ufl.dot(ufl.grad(tmpvar),ufl.grad(testvar))*ufl.dx+tmpvar*testvar*ufl.dx
-    return a
+        d=consts_dict['d_t']
+    D=Constant(d*al)#D  =fem.Constant(r_mesh, PETSc.ScalarType(d))
+    a1=D*dt*inner(grad(tmpvar),grad(testvar))*ufl.dx +tmpvar*testvar*ufl.dx  
+    return a1
 
 def convert_vec_to_array(vec,nx,ny):
     arr=np.zeros((nx+1,ny+1))
@@ -184,21 +184,17 @@ def Simulations(filename,path,MeshFolder,ResultsFolder,varnames,test=0,ind=0):
     hy=(ym-y0)/ny # mesh size
     boundaries=np.array([x0,y0,xm,ym])
     # Parameters for the PDEs
-    global StSt, perm
+    #global StSt, perm
     StSt=pdes_par_dict['StSt']
-    eps=pdes_par_dict['eps']
-    A0=pdes_par_dict['A0']
-    d=pdes_par_dict['d']
     dt=pdes_par_dict['dt']
     T=pdes_par_dict['T']
     perm=pdes_par_dict['perm']
-    parstr=ff.par_string(ind,dt,T,perm,hx,hy,A0,d,eps)
+    parstr=ff.par_string(ind,pdes_par_dict)
     MeshFolder=MeshFolder+parstr
     ResultsFolder=ResultsFolder+parstr
     ff.check_folder(path,ResultsFolder)
     ff.check_folder(path,MeshFolder)    
     # Simulation parameters--------------------------------------------
-    h = max(hx,hy)      # mesh size
     nt=int(T/dt)
     tvec = np.arange(0,T,dt)
 
@@ -214,7 +210,6 @@ def Simulations(filename,path,MeshFolder,ResultsFolder,varnames,test=0,ind=0):
 
     # FeNiCs variables--------------------------------------------------
     # Initialise the space ---------------------------------------------
-    Ns=(nx,ny)
     r_mesh=RectangleMesh(nx,ny,xm-x0,ym-y0)
     V = FunctionSpace(r_mesh, 'CG',1)
     # Function(V) indicates a scalar variable over the domain.
@@ -242,14 +237,14 @@ def Simulations(filename,path,MeshFolder,ResultsFolder,varnames,test=0,ind=0):
     
     # Define one-step variational problem ---------------------------------------
     vars_cls=(eta0_cls,the0_cls)
-    Lvec,avec=first_step(vars_cls,pdes_par_dict,test)
+    #Lvec,avec=first_step(vars_cls,pdes_par_dict,test)
     eta1_cls=sv.spatial_variable(V,r_mesh,etaname+'1')
     eta1_cls.set_char_key('e')
     the1_cls=sv.spatial_variable(V,r_mesh,thetaname+'1')
     the1_cls.set_char_key('t')
 
     Outfiles=(etaout,theout)
-    vars_cls, eNorm, tNorm,eDiv,tDiv,tvec,vargs=one_step_loop(vars_cls,Lvec,avec,pdes_par_dict,tvec,path,ResultsFolder,Outfiles)
+    vars_cls, eNorm, tNorm,eDiv,tDiv,tvec,vargs=one_step_loop(vars_cls,pdes_par_dict,tvec,path,ResultsFolder,Outfiles,test)
     eta2_cls,the2_cls=vars_cls
     #plotting.plot(eta1)
     # Save the outputs from the simulation
@@ -295,15 +290,15 @@ def first_step(vars_cls,consts_dict,test=1):
     v1      = TestFunction(V)
     L0=linear_form_func(var0_cls.char_key,var0_cls,var1_cls,v0,consts_dict,test) # Linear form for eta with time step 1
     L1=linear_form_func(var1_cls.char_key,var1_cls,var0_cls,v1,consts_dict,test)
-    a0=bilinear_form_func(var0_cls.char_key,tmpv0,v0,consts_dict,r_mesh,test=0)
-    a1=bilinear_form_func(var1_cls.char_key,tmpv1,v0,consts_dict,r_mesh,test=0)
+    a0=bilinear_form_func(var0_cls.char_key,tmpv0,v0,consts_dict,test)
+    a1=bilinear_form_func(var1_cls.char_key,tmpv1,v0,consts_dict,test)
     
     Lvec=(L0,L1)
     avec=(a0,a1)
     return Lvec,avec
 
 
-def one_step_loop(vars_cls,Lvec,avec,consts_dict,tvec,path,MeshFolder="",Outfiles=()):
+def one_step_loop(vars_cls,consts_dict,tvec,path,MeshFolder="",Outfiles=(),test=0):
     ''' Solve the spatial problem for each time step using the one step solver. 
     Solutions are plotted at each time step and the output is the variable with the values for the final time step along with the resiudal vector.
     '''
@@ -315,8 +310,6 @@ def one_step_loop(vars_cls,Lvec,avec,consts_dict,tvec,path,MeshFolder="",Outfile
     var11_cls=sv.spatial_variable(V,r_mesh,var10_cls.name+'1')
     var11_cls.set_char_key(var10_cls.char_key)
     #Solver_0,Solver_1=Solvers
-    L0,L1            =Lvec
-    a0,a1            =avec
     Norm0=np.zeros(len(tvec))
     Norm1=np.zeros(len(tvec))
     Div0=np.zeros(len(tvec))
@@ -338,12 +331,20 @@ def one_step_loop(vars_cls,Lvec,avec,consts_dict,tvec,path,MeshFolder="",Outfile
     check_out=False
     # Create empty lists to store results in
     rescount=0
-    res_fac=20
+    res_fac=0
     tstep=0
     vmin0=min(var00_cls.var_vals())
     vmax0=max(var00_cls.var_vals())
     vmin1=min(var10_cls.var_vals())
     vmax1=max(var10_cls.var_vals())
+    spars={'snes_monitor': None,
+            'snes_max_it': 300,
+            "snes_type": "newtonls",
+            "ksp_type" : "gmres",
+            "pc_type" : "lu"}
+    Lvec,avec=first_step((var00_cls,var10_cls),consts_dict,test)
+    L0,L1=Lvec
+    a0,a1=avec
     for tstep,t in enumerate(tvec):
         if check_out or np.isnan(res_fac):
              # Nans in output solution blown up
@@ -354,7 +355,7 @@ def one_step_loop(vars_cls,Lvec,avec,consts_dict,tvec,path,MeshFolder="",Outfile
             print('Time steps exceeded maximum')
             print('Final time %.3f on step %d'%((t-dt),tstep))
             break
-        elif res_fac>1E+200 :
+        elif res_fac>1E+5 :
             print('Residual blown up')
             print('Final time %.3f on step %d'%((t-dt),tstep))
             break
@@ -366,8 +367,12 @@ def one_step_loop(vars_cls,Lvec,avec,consts_dict,tvec,path,MeshFolder="",Outfile
                 break
         #print('Solving for time step',t)
         # Update the right hand side reusing the initial vector
-        solve(a0==L0,var01_cls.var)
-        solve(a1==L1,var11_cls.var)
+        try:
+            solve(a0==L0,var01_cls.var,solver_parameters=spars)
+            solve(a1==L1,var11_cls.var,solver_parameters=spars)
+        except:
+            print('Solve failed at step',tstep)
+            break
         
         varlist=(var01_cls.var_vals(),var11_cls.var_vals())
         check_out=check_output(varlist)
@@ -424,20 +429,24 @@ def Plotting(var_cls,nt,path,PlotFoldername,parstr,consts_dict,vargs):
     vmin0,vmax0=vargs
     fig, axes = plt.subplots()
     axes.set_aspect('equal')
-    colors = tripcolor(var_init, num_sample_points=1, axes=axes,cmap='inferno',vmin=vmin0,vmax=vmax0)
+    colors = tripcolor(var_init, num_sample_points=1, axes=axes,cmap='inferno')
+    #vmin=vmin0,vmax=vmax0)
     fig.colorbar(colors)
-    fig.savefig(path+PlotFoldername+parstr+'/'+var_cls.name+'000.jpg')
+    fig.savefig(path+PlotFoldername+parstr+'/'+var_cls.name+ff.numfix(0)+'.jpg')
     fig.clear()
+    plt.close()
     for tstep in range(nt-1):
         fig, axes = plt.subplots()
         axes.set_aspect('equal')
-        colors = tripcolor(var_cls.var_hist[tstep], num_sample_points=1, axes=axes,cmap='inferno',vmin=vmin0,vmax=vmax0)
+        colors = tripcolor(var_list[tstep], num_sample_points=1, 
+        axes=axes,cmap='inferno')#,vmin=vmin0,vmax=vmax0)
         fig.colorbar(colors)
-        fig.savefig(path+PlotFoldername+parstr+'/'+var_cls.name+'%.3d.jpg'%tstep)
+        fig.savefig(path+PlotFoldername+parstr+'/'+var_cls.name+ff.numfix(tstep)+'.jpg')
         fig.clear()
+        plt.close()
     return
 
-def main(path,filename,simon=0,test=0,ind=0):
+def main(path,filename,simon=0,test=0,ind=0,simstep=1):
     '''Runs the simulations and plotting functions.
     Ouputs from the simulation are saved as numpy variables. These are then loaded before plotting.
     :param filename: The name of the file containing the input parameters
@@ -447,8 +456,8 @@ def main(path,filename,simon=0,test=0,ind=0):
     Uses functions :py:func:`Simulations(filename,MeshFolder,ResultsFolder,varnames,test)' and :py:func:`plotting(var1,var2,time,foldername)'
     :returns: nothing
     '''
-    ResultsFolder='PlotsFirstOrder/'
-    MeshFolder  ='MeshFirstOrder/'
+    ResultsFolder='Plots'+ff.simstr(simstep)+'/'
+    MeshFolder  ='Mesh'+ff.simstr(simstep)+'/'
     dirlist=(ResultsFolder,MeshFolder)
     dlistout=list()
     for d in dirlist:
@@ -473,12 +482,14 @@ def main(path,filename,simon=0,test=0,ind=0):
     for i,var in enumerate(varlist):
         Plotting(var,nt,path,ResultsFolder,parstr,consts_dict,varr[i])
     #MeshFolder=MeshFolder+parstring
-    #ResultsFolder=ResultsFolder+parstring
+    #ResultsFolder=ResultsFolder+parstring    tvec=ff.load_file(path,MeshFolder+parstr,tname)
     varlist=(etaresname,theresname,etadivname,thedivname)
     for vname in varlist:
         var=ff.load_file(path,MeshFolder+parstr,vname)
         pf.line_plot(var,np.log(tvec+1),vname,'log'+tname+'Plus1',path,ResultsFolder+'/'+parstr)
-        del var
+    for vname in varlist:
+        var=ff.load_file(path,MeshFolder+parstr,vname)
+        pf.line_plot(var,tvec,vname,tname,path,ResultsFolder+'/'+parstr)
     etadiv=ff.load_file(path,MeshFolder+parstr,etadivname)
     thediv=ff.load_file(path,MeshFolder+parstr,thedivname)
     pf.line_plot(etadiv,thediv,etadivname,thedivname,path,ResultsFolder+'/'+parstr)
